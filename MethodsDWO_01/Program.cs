e9cff911ef1beb3ab6d9ac084b4ea2fb1cf2a8a4
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-02-28 14:27:52 +0300

    .

---
+++ MethodsDWO_01/Program.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_EmeklilikHesaplama
+{
+    internal class Program
+    {
+        static int pDayCount;
+
+        //Erkeklerin 25 yıl, kadınların da 20 yıl çalıştıktan sonra emekli olacağı varsayılan bir ortamda, klavyeden girilen cinsiyet ve sigorta prim gün sayısına göre kişinin emekliliğine ne kadar süresi kaldığını bulan metodunu yazınız.
+
+        // Açıklama: 1 yıl = 360 gün 1 ay = 30 gün  Baz alınacaktır.
+        // Örnek: 1943 gün = 5 yıl 4 ay 23 gün , Bu örnekteki metoda cinsiyet parametresi de ekleyerek, buna göre sonucu ekrana yazdırınız.
+        //Örnek: Erkek ve 6643 gün parametreleri girildiğinde "Emekliliğinize 6 yıl 6 ay 17 gün kaldı" şeklinde sonuç döndürecek.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Cinsiyet Bilgisi Giriniz : ");
+            string cinsiyet = Console.ReadLine();
+            emekliHesapla(cinsiyet);
+
+
+            //int Year = 360;
+            //int Month = 30;
+            //int pDayCount; // prim gün sayısı
+
+            //string gender;
+
+            //Console.WriteLine("Lütfen Cinsiyet Bilgisi Giriniz (E/K)   : ");
+
+            //gender = Console.ReadLine();
+            //Console.WriteLine("Lütfen Prim Gün Sayısı Giriniz : ");
+            //pDayCount = Convert.ToInt32(Console.ReadLine());
+            //int year = retirementCalculate(gender, pDayCount) / Year;
+            //pDayCount %= Year;
+
+            //int month = retirementCalculate(gender, pDayCount) / Month;
+            //pDayCount %= Month;
+
+            //int day = pDayCount;
+
+            //Console.WriteLine($"\n\nEmekliliğinize {year} yil , {month} ay , {day} gün kalmıştır...");
+            Console.ReadLine();
+        }
+        private static void calismaSureHesapla()
+        {
+
+        }
+        private static string emekliHesapla(string cinsiyet)
+        {
+            Console.WriteLine("Lütfen Sigorta Prim Gününüzü Giriniz :");
+            int sgk = Convert.ToInt32(Console.ReadLine());
+
+            int yil = (sgk / 360);
+            int ay = (sgk % 360) / 30;
+            int gun = (sgk % 360) % 30;
+
+            Console.WriteLine("Çalışma Süreniz : {0}", (yil + "yil , " + ay + "ay , " + gun + "gün'dür.."));
+
+            if (cinsiyet.ToLower() == "e")
+            {
+                if (sgk >= 9000)
+                {
+                    Console.WriteLine("Çalışmalarınızdan dolayı tebrik ederiz. Emekliliğin tadını çıkarın beyfendii ... ");
+                }
+                else if (sgk < 9000)
+                {
+                    int kalanGun = 9000 - sgk;
+                    int yil1 = kalanGun / 360;
+                    int ay1 = (kalanGun % 360) / 30;
+                    int gun1 = (kalanGun % 360) % 30;
+                    Console.WriteLine("Emekli Olmanız İçin Gereken Prim Gün Sayısı : {0}\nEmeklili olmanıza kalan Süre : {1} 'dür..\nSabredin Günler Çabuk Geçip Gider ,\nÇalışmak ise Vücudu Dinç Eyler...  ", kalanGun, (yil1 + "yil," + ay1 + "ay," + gun1 + "gun"));
+                }
+            }
+            else if (cinsiyet.ToLower() == "k")
+            {
+                if (sgk >= 7200)
+                {
+                    Console.WriteLine("Çalışmalarınızdan dolayı tebrik ederiz. Emekliliğin tadını çıkarın hanımefendii ... ");
+                }
+                else if (sgk < 7200)
+                {
+                    int kalanGun = 7200 - sgk;
+                    int yil1 = kalanGun / 360;
+                    int ay1 = (kalanGun % 360) / 30;
+                    int gun1 = (kalanGun % 30) % 30;
+                    Console.WriteLine("Emekli Olmanız İçin Gereken Prim Gün Sayısı : {0}\nEmeklili olmanıza kalan Süre : {1} 'dür..\nSabredin Günler Çabuk Geçip Gider ,\nÇalışmak ise Vücudu Dinç Eyler...  ", kalanGun, (yil1 + "yil," + ay1 + "ay," + gun1 + "gun"));
+                }
+            }
+
+            return cinsiyet;
+        }
+
+        private static int retirementCalculate(string prmGender, int day)
+        {
+            int dayForWoman = 7200;
+            int dayForMan = 9000;
+
+            switch (prmGender)
+            {
+                case "E":
+                    day = dayForMan - pDayCount;
+                    break;
+                case "K":
+                    day = dayForWoman - pDayCount;
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            return day;
+        }
+    }
+}
---
+++ MethodsDWO_02/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDWO_02
+{
+    internal class Program
+    {
+        // Global Degiskenler..
+
+        // datUsers
+        public static string[,] datUsers = new string[,]
+        {
+            {"admin", "ferit", "cptmfs"},  // kullanıcı adları
+            {"admin", "1234", "5678"},     // Şifreler
+            {"ADMİN", "User", "User"},     // Kullanıcı Türü
+            {"+"    , "-"   , "-"},        // Kullanıcı Yetkileri
+        };
+        public static string userAuthority; // Admin yada User 
+        public static string userStatus;    // Kullanıcı Aktif mi Pasif mi ? 
+
+        
+        static void Main(string[] args)
+        {
+            //kullanıcı adi , sifresi , yetkisi , aktifliğini iceren bir dizi var.
+            // (User Tablosu Gibi )
+            // 4x3 boyutunda manuel  (hard coded) olusturulacak..
+            // K1,K2,K3 -- kullanıcı adları
+            // P1,P2,P3 -- sifreleri
+            // A ,K ,K  -- yetki derecesi ( Admin'mi normal kullanıcımı?)
+            // + ,+ ,+  -- kullanıcının aktifliği ( + aktif , - pasif )
+            //
+            // bunun disinda sistem üzerindeki islemleri izleyebilecegim ( Admin'in ) bir log dizisi var. ( Tek Boyutlu )
+            //
+            // 1. Program ilk calıstıgında kullanıcıyı bir  Ana Menu karsılayacak (Metodsal Yapı).. Secenekler 1. Sisteme Giris , 2. Cıkıs..
+            // 2. Sisteme Giris'e basildiginda Kullanıcının karsisina bir login ekranı gelmeli.
+            // 3. Kullanıcıdan userName ve passWord istenecek..
+            // 4. Bir kontrol islemiyle(datUser dizisi) kullanıcının var olup olmadıgı kontrol edilecek.. Yok ise ;   Uyarı Mesajı verilecek.. ve Tekrar Login Ekranına Gönderilecek.. Var ise ; Kullanıcının diger bilgileri (yetki ve aktiflik) ögrenilecek.. Buna göre yetki durumuna baglı olarak farklı ekranlar gösterilecek.. Yani ; Admin ise (Admin Paneli) User ise (Yapacağı İşlemler Listesi) gibi..
+            // 5. Yapılan işlemler datLog isimli bir diziye işlenecek.. Admin istediğinde bu kayıtlar listelenecek.. Bu Secenek Admin Menusunde olacak..
+            
+            bool showMenu = true;
+
+            while (showMenu)
+            {
+                showMenu = mainMenu();
+
+            }
+            Console.WriteLine("Çıkış için herhangi bir tuşa basınız...");
+            Console.ReadKey();
+
+        }
+
+        private static bool mainMenu()
+        {
+            bool showLogin = true;
+            Console.Clear();
+            Console.WriteLine("\t\t\t\t\t\t ------ Ana Menü ------ ");
+            Console.WriteLine("1. Sistem Giriş");
+            Console.WriteLine("2. Programdan Çıkış");
+            Console.Write("\nSeçeneğiniz : ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    while (showLogin)
+                    {
+                        showLogin = loginMenu();
+                    }
+                    return true;
+
+                case "2":
+
+                    //Programdan Cikis bolumu..
+                    return false;
+
+                default:
+                    return true;
+
+            }
+
+
+        }
+        private static bool loginMenu()
+        {
+            string userID = "", userPass = "";
+            Console.WriteLine("\t\t\t\t\t\t ------ Kullanıcı Girişi ------ ");
+            Console.WriteLine("Lütfen Kullanıcı adını giriniz : ");
+            userID = Console.ReadLine().Trim();
+            Console.WriteLine("Lütfen Şifrenizi giriniz : ");
+            userPass = Console.ReadLine();
+            userPass = userPass.Trim();
+
+            if (UserControl(datUsers,userID,userPass))
+            {
+                // eger kullanıcı bulunduysa ..
+                Console.WriteLine("Kullanıcı Girişi Başarılı!   \n\n");
+                Console.WriteLine($"Kullanıcı Yetki Seviyesi  : {userAuthority}\n\n");
+                Console.WriteLine($"Kullanıcı Durumu  : {userStatus}\n\n");
+                Console.WriteLine("Devam Etmek için bir tuşa basınız...");
+
+            }
+            else
+            {
+                Console.WriteLine("Kullanıcı Girişi Başarısız..");
+            }
+            Console.ReadKey();
+
+
+            return true;
+        }
+        private static bool UserControl(string[,]prmUsers,string prmUser,string prmPass)
+        {
+
+            bool found = false; // Kullanıcının bulunup bulunmadıgını belirten bir degisken
+
+            // içiçe for döngüleri yardımıyla  tüm dizi  ögelerini dolasalım.
+
+            for (int satir = 0; satir < prmUsers.GetLength(0); satir++)
+            {
+                for (int sutun = 0; sutun < prmUsers.GetLength(1); sutun++)
+                {
+                    if (prmUsers[satir,sutun] == prmUser) // eslesen kullanıcı varmı ? 
+                    {
+                        if (prmUsers[satir+1, sutun] == prmPass) // kullanıcının alt satırında aynı sutundaki sifre eslesiyormu ?
+                        {
+                            userAuthority = prmUsers[satir + 2, sutun]; // kullanıcı adı sifre eslesen yerde kullanıcı adının 2 satır altında tanımladıgımız . Admin User gibi kullanıcı  Türünü atıyoruz.
+                            userStatus = prmUsers[satir + 3, sutun]; // kullanıcı adı sifre eslesen yerde kullanıcı adının 3 satır altında tanımladıgımız . + / - gibi kullanıcı  aktiflik pasiflik durumunu atıyoruz.
+
+                            found = true; // Kullanıcıyı buldum. 
+                            break;  // döngüden sonlandır..
+                        }
+                    }
+
+                }
+                if (found == true)
+                {
+                    break;
+                }
+            }
+            return found;
+        }
+    }
+}

3d5e35a59876d0a21787832a24431a65c80ca53b
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-03-03 11:23:05 +0300

    .

--- 14_Encapsulation/Program.cs
+++ 14_Encapsulation/Program.cs
@@ -1,7 +1,32 @@
-﻿internal class Program
+﻿using _14_Encapsulation;
+
+internal class Program 
 {
     private static void Main(string[] args)
     {
-        
+        #region Normal Durum
+
+        clsBank clsBank = new clsBank();
+
+        clsBank.accountNumber = 1234567890;
+        clsBank.accountName = "Satoshi Bank";
+        clsBank.balance = 3500000;
+
+        clsBank.GetBalance();
+        clsBank.WithdrawAccount();
+        clsBank.Deposit();
+
+        #endregion
+        Console.WriteLine("\n-------------------------------------------\n");
+        #region Getter / Setter
+
+
+        clsBank.SetBalanceCaps(1500);
+        Console.WriteLine($"Balance size : {clsBank.GetBalanceCaps()}");
+
+        #endregion
+
+
+        Console.ReadKey();
     }
 }
---
+++ 14_Encapsulation/clsBank.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Encapsulation
+{
+    internal class clsBank
+    {
+
+        #region Normal Durum
+
+        public long accountNumber; // Hesap No..
+        public string accountName; // Hesap Adı..
+        public int balance; // Hesaptaki Miktar..
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"Hesap Numarası : {accountNumber}\tHesap Adı : {accountName}\tHesap Bakiyesi : {balance}");
+        }
+
+        public void WithdrawAccount()
+        {
+            int withdraw = balance - 50000;
+
+            Console.WriteLine($"Hesapdan para çekme işlemi gerçekleştirildi\nYeni Hesap Bakiyesi : {withdraw}");
+        }
+
+        public void Deposit()
+        {
+            int deposit = balance + 100000000;
+            Console.WriteLine($"Hesaba para yatırma işlemi gerçekleştirildi\nYeni Hesap Bakiyesi : {deposit}");
+
+        }
+
+
+        #endregion
+
+
+        #region Getter / Setter
+
+        private double balance2; // uani burada dışarıya kapalı bir degisken tanımı var 
+
+        //set / get metotlarının tanımlanması...
+
+        // Public getter metot -> Bu metot balance2 değişkenimizde tutulan  değeri almak için 
+
+        public double GetBalanceCaps() // private olan değişkeni public bir metotda yer verdiğimiz zaman onu sanki public bir değişkenmiş                                    gibi kullanabiliriz... 
+        {
+            //Burada farklı işlemler var.. balance2 = balance2 * 1.15 ; gibi 
+
+            return balance2;
+        }
+
+        // Public setter metot -> Bu metot balance2 değişkenimizde tutulan  değeri değiştirmek/güncellemek için kullanılır..
+
+        public void SetBalanceCaps(double balance2)
+        {
+            // Burada yine farklı işlemler olabilir.. balance2 = balance2 * 1.15 ; gibi 
+
+            this.balance2 = balance2;  // " this " Benim yukarda tanımladığım değişkenimi güncelleyeceksin demek..
+        }
+
+
+
+        #endregion
+
+    }
+}
